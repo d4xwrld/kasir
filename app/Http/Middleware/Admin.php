daa311d9f9d3533f98f094676e70418fe4c55c94
d4xwrld <reyzcraft87@gmail.com>
2024-08-01 11:21:14 +0700

    add permission only admin can access /register

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -17,7 +17,7 @@ class Admin
     public function handle(Request $request, Closure $next): Response
     {
         if(Auth::user()->usertype != 'admin'){
-            return redirect('admin/dashboard');
+            return redirect('dashboard');
         }
         return $next($request);
     }
--- routes/auth.php
+++ routes/auth.php
@@ -9,13 +9,13 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register')->middleware(['auth', 'admin']);
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+
+
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
@@ -36,6 +36,7 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+
     Route::get('verify-email', EmailVerificationPromptController::class)
                 ->name('verification.notice');
 
@@ -56,4 +57,11 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
+});
+
+Route::middleware('admin')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+    ->name('register')->middleware(['auth', 'admin']);
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
 });
